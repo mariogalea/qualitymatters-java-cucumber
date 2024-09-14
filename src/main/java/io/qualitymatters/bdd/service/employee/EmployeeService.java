127da35193e86ce64c82bd3d5a8274f42fb2ce3d
MarioG <mario.galea84@gmail.com>
2024-09-14 17:08:28 +0200

    Fix OKHttp lib in maven.  Specify constructor ain EmployeeService.java

---
+++ src/main/java/io/qualitymatters/bdd/service/employee/EmployeeService.java
@@ -0,0 +1,20 @@
+package io.qualitymatters.bdd.service.employee;
+
+
+import com.sun.net.httpserver.Request;
+
+/*
+
+ */
+public class EmployeeService {
+
+    public EmployeeService() {
+
+    }
+
+    public void getEmployeeList() {
+
+
+    }
+
+}
