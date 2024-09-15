bc001f91eaf95f7e855d56717fcde16bbb7574b3
MarioG <mario.galea84@gmail.com>
2024-09-15 02:47:35 +0200

    Build a GET request for all bookings.

--- src/main/java/io/qualitymatters/bdd/service/booking/BookingService.java
+++ src/main/java/io/qualitymatters/bdd/service/booking/BookingService.java
@@ -1,12 +1,49 @@
 package io.qualitymatters.bdd.service.booking;
 
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 /*
 
  */
+
+
 public class BookingService {
 
     public BookingService() {
+
+    }
+
+    public HttpResponse<String> getBookings() throws IOException, InterruptedException {
+
+        // HTTP Client
+        HttpClient client = HttpClient
+                .newBuilder()
+                .connectTimeout(Duration.ofMillis(1000))
+                .build();
+
+
+
+        // Request - GET HTTP
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("https://restful-booker.herokuapp.com/booking"))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+        // Response - Store Response
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.printf("Status %s \n", response.statusCode());
+        System.out.printf("Response %s \n", response.body());
+
+        return response;
+
 
     }
 
--- src/test/java/booking/StepDefinitions.java
+++ src/test/java/booking/StepDefinitions.java
@@ -1,15 +1,26 @@
 package booking;
 
 import io.cucumber.java.en.*;
+import io.qualitymatters.bdd.service.booking.BookingService;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
 
 public class StepDefinitions {
 
+    BookingService service = new BookingService();
+
     @Given("a bookings list is available")
-    public void aBookingsListIsAvailable() {
+    public void aBookingsListIsAvailable() throws IOException, InterruptedException {
+
+        service.getBookings();
+
     }
 
     @When("the user retrieves booking list")
-    public void theUserRetrievesBookingList() {
+    public void theUserRetrievesBookingList() throws IOException, InterruptedException {
+
+        HttpResponse<String> bookingsList = service.getBookings();
 
     }
 
@@ -19,8 +30,12 @@ public class StepDefinitions {
     }
 
     @Then("the user should have a list of all bookings")
-    public void theUserShouldHaveAListOfAllBookings() {
+    public void theUserShouldHaveAListOfAllBookings() throws IOException, InterruptedException {
 
+        HttpResponse<String> bookingsList = service.getBookings();
+
+
+        assert !(bookingsList == null);
     }
 
     @Then("the user should have booking id {int}")
