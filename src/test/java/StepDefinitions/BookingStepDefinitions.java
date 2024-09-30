2ad67a5df4e78f4202898dc294c52c77be3bab4b
MarioG <mario.galea84@gmail.com>
2024-09-30 18:20:45 +0200

    Finalise Test by completing assertion

--- src/test/java/StepDefinitions/BookingStepDefinitions.java
+++ src/test/java/StepDefinitions/BookingStepDefinitions.java
@@ -3,13 +3,14 @@ package StepDefinitions;
 import io.cucumber.java.en.*;
 import io.qualitymatters.bdd.pojo.BookingId;
 import io.qualitymatters.bdd.service.booking.BookingService;
+import org.junit.jupiter.api.Assertions;
 
 import java.io.IOException;
 import java.util.List;
 
 public class BookingStepDefinitions {
 
-    private Context context;
+    private final Context context;
 
     public BookingStepDefinitions(Context context) {
         this.context = context;
@@ -39,12 +40,14 @@ public class BookingStepDefinitions {
     @Then("the user should have a list of all bookings")
     public void theUserShouldHaveAListOfAllBookings() throws IOException, InterruptedException {
 
-        // Dependency Inject the bookingIds to assert that there are ids available.
         List<BookingId> bookingIds = context.bookingIds;
-
+        /*
         for (BookingId i : bookingIds) {
             System.out.printf("Booking Id =  %s \n", i.getBookingid());
         }
+        */
+        Assertions.assertFalse(bookingIds.isEmpty());
+
 
     }
 
